56d215ad94dc337de6bcebcd7caf03a10282a0c3
willmer <willmer.orsetti@cashclick.mx>
2022-09-13 18:44:30 -0500

    subo codigo java jax funciona tomcat

---
+++ JAX-RS-JSON/src/main/java/com/willmer/rest/jersey/server/RestServer.java
@@ -0,0 +1,35 @@
+package com.willmer.rest.jersey.server;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.willmer.data.model.Student;
+
+@Path("/student/data1")
+public class RestServer {
+
+	 @GET
+	    @Path("/get")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Student getStudentRecord(){
+	        Student student = new Student();
+	        student.setFirstName("Elizabeth");
+	        student.setLastName("Hayden");
+	        student.setSchool("Little Flower");
+	        student.setStandard("One");
+	        student.setRollNumber(1113);
+	        return student;
+	    }
+	     
+	    @POST
+	    @Path("/post")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    public Response postStudentRecord(Student student){
+	        String result = "Record entered: "+ student;
+	        return Response.status(201).entity(result).build();
+	    }
+	}
